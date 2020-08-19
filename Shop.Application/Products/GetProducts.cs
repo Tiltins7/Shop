cafca3afea0ff92d0a444ab219e10a668ecf6fbf
Kristers <you@example.com>
2020-08-19 18:09:03 +0300

    Importing Project

---
+++ Shop.Application/Products/GetProducts.cs
@@ -0,0 +1,42 @@
+﻿using Shop.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shop.Application.Products
+{
+    public class GetProducts
+    {
+        private ApplicationDbContext _ctx;
+
+        public GetProducts(ApplicationDbContext ctx)
+        {
+            _ctx = ctx; 
+        }
+
+        public IEnumerable<ProductViewModel> Do() => 
+            _ctx.Products.ToList().Select(x => new ProductViewModel
+            {
+                Name = x.Name,
+                Description = x.Description,
+                Value = $"${x.Value.ToString("N2")}", //1100.50 converting to this 1,100.50 
+
+            });
+        
+
+        public class ProductViewModel
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string Value { get; set; }
+        }
+
+		public class StockViewModel
+		{
+			public int Id { get; set; }
+			public string Description { get; set; }
+			public int Qty { get; set; }
+		}
+	}
+}
---
+++ Shop.UI/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shop.Application.Products;
+using Shop.Application.ProductsAdmin;
+using Shop.Database;
+using GetProducts = Shop.Application.Products.GetProducts;
+
+namespace Shop.UI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private ApplicationDbContext _ctx;
+
+        public IndexModel(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<GetProducts.ProductViewModel> Products { get; set; }
+
+
+        public void OnGet()
+        {
+            Products = new GetProducts(_ctx).Do();
+        }
+
+    }
+}
